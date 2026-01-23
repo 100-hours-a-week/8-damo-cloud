cad55512f71eaeaf496dcbbca27f990637d31277
tn-als <kusumin38@gmail.com>
2026-01-23 09:02:43 +0900

    feat: prod/dev EC2, SG Terraform 구성

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -7,6 +7,24 @@ module "network" {
   private_subnets = var.private_subnets
 }
 
+module "security" {
+  source = "../../modules/security"
+
+  name = "prod-damo-public"
+  vpc_id = module.network.vpc_id
+}
+
+module "ec2" {
+  source = "../../modules/ec2"
+
+  name                    = "prod-damo-v1"
+  ami                     = var.instance_ami
+  instance_type           = "t4g.medium"
+  key_name                = var.instance_key_name
+  subnet_id               = element(values(module.network.public_subnet_ids), 0)
+  vpc_security_group_ids  = [module.security.security_group_id]
+}
+
 output "prod_vpc_id" {
   value = module.network.vpc_id
 }
---
+++ terraform/modules/security/security_group.tf
@@ -0,0 +1,70 @@
+variable "name" {
+  description = "Name for the security group"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the security group will be created"
+  type        = string
+}
+
+resource "aws_security_group" "this" {
+  name        = var.name
+  description = var.name
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "https"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "ai"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "fe"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "be"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
